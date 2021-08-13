aa4d49d49b6cc6bc28050583de1d88595999c7de
taangja1 <jan.nguyen@swisscom.com>
2021-08-13 13:22:59 +0200

    old navbar

--- potraitwebsite/src/components/Navbar/index.js
+++ potraitwebsite/src/components/Navbar/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FaBars} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Sidebar from '../Sidebar/index';
 import './Navbar.css';
 
+
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    
+    const handleClick = () => setClick(!click);
+
     return (
         <>
            <div className="Nav">
                <div className="NavbarContainer">
-                   <div className="NavLogo">
+                   <div className="NavLogo" >
                        Nguyen Jan
+                   </div>
+                   <div className="MobileIcon" onClick={handleClick}>
+                        <FaBars />
+                   </div>
+                   <div className="NavMenu">
+                       <div className="NavITem">
+                           <Link className="NavLinks">
+                                About Me
+                           </Link>
+                       </div>
+                       <div className="NavITem">
+                           <div className="NavLinks" to='about'>
+                                Experience
+                           </div>
+                       </div>
+                       <div className="NavITem">
+                           <div className="NavLinks" to='about'>
+                                Skills
+                           </div>
+                       </div>
+                       <div className="NavITem">
+                           <div className="NavLinks" to='about'>
+                                FreeTime
+                           </div>
+                       </div>
                    </div>
                </div>
            </div>
---
+++ potraitwebsite/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+export const Home = () => {
+    
+    return (
+        <>
+            <Navbar /> 
+            <Sidebar />
+        </>
+    )
+}
+
+export default Home
